33e94f8e0fc3c39f7cbd1337da755742de820e16
Ricardo Meza <rihard93@icloud.com>
2018-03-30 13:38:46 -0600

    Carga Clase Principal

--- miniPHP/src/miniphp/miniphp.java
+++ miniPHP/src/miniphp/miniphp.java
@@ -4,18 +4,27 @@
  * and open the template in the editor.
  */
 package miniphp;
+import java.io.File;
 
 /**
  *
  * @author Ricardo
  */
-public class MiniPHP {
+public class miniphp {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String Ruta = "C:/Users/Ricardo/Documents/Lenguajes/miniphp/src/miniphp/MyRegEx.txt";
+        GenerarLexer(Ruta);
     }
     
+    public static void GenerarLexer(String path)
+    {
+        File file = new File(path);
+        jflex.Main.generate(file);
+    }
 }
